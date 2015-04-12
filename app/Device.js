951abcd95bed94644083b298e4fa4465b1474932
Gertjan Reynaert <gertjan@typework.com>
2015-04-12 11:07:18 +0200

    improved Device class

--- app/Device.js
+++ app/Device.js
@@ -4,20 +4,43 @@ class Device {
   constructor() {
     this.width = this._width();
     this.height = this._height();
+    this.kind = this._kind();
   }
 
   isIpad() {
-    var iPad = [768, 1024];
-    return iPad.indexOf(this.width) > -1 && iPad.indexOf(this.height) > -1;
+    return this.kind.indexOf("iPad") === 0;
   }
 
   isIphone() {
+    return this.kind.indexOf("iPhone") === 0;
+  }
+
+  _kind() {
+    var iPad = [768, 1024];
     var iPhone4 = [320, 480];
     var iPhone5 = [320, 568];
     var iPhone6 = [375, 667];
     var iPhone6plus = [414, 736];
-    var iPhone = iPhone4.concat(iPhone5).concat(iPhone6).concat(iPhone6plus);
-    return iPhone.indexOf(this.width) > -1 && iPhone.indexOf(this.height) > -1;
+
+    if (iPad.indexOf(this.width) > -1 && iPad.indexOf(this.height) > -1) {
+      return "iPad";
+    }
+
+    if (iPhone4.indexOf(this.width) > -1 && iPhone4.indexOf(this.height) > -1) {
+      return "iPhone4";
+    }
+
+    if (iPhone5.indexOf(this.width) > -1 && iPhone5.indexOf(this.height) > -1) {
+      return "iPhone5";
+    }
+
+    if (iPhone6.indexOf(this.width) > -1 && iPhone6.indexOf(this.height) > -1) {
+      return "iPhone6";
+    }
+
+    if (iPhone6plus.indexOf(this.width) > -1 && iPhone6plus.indexOf(this.height) > -1) {
+      return "iPhone6plus";
+    }
   }
 
   _width() {
